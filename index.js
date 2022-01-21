21c94071cc83238c533e107540578a79ce67a6dd
Pat5657 <43613117+Pat5657@users.noreply.github.com>
2022-01-21 00:50:39 +0000

    Added image scraping and other changes. - Added dotenv.

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+require('dotenv').config()
 let openRentScraper = require('./scrapers/openRentScraper')
+let axios = require('axios')
 
-// Define scrape targets
-const targets = [
-    {
-        site: 'openrent',
-        url: 'https://www.openrent.co.uk/properties-to-rent/wembley-greater-london?term=Wembley,%20Greater%20London&prices_max=2700&bedrooms_min=3&bedrooms_max=4&hasParking=true'
-    }
-]
+
+const api = axios.create({
+    baseURL: process.env.API_URL
+})
 
 // define main function
 let run = async () => {
+    console.log('~~~~~~ Prepare for hunt ~~~~~~')
+
+    // retrieve token
+    let { data, status } = await api.post('/auth/login', {
+        username: 'pat',
+        password: 'Admin123!'
+    })
+    // validate login api call
+    if (status != 200) {
+        console.log('login failed')
+        return
+    }
+    console.log('Logged in')
+    // extract auth token 
+    const { token: AUTH_TOKEN } = data
+    // set api token
+    api.defaults.headers.common['Authorization'] = 'Bearer ' + AUTH_TOKEN
+
+    // fetch targets
+    let { data: targets, status: targetsStatus} = await api.get('/target')
+    // validate login api call
+    if (targetsStatus != 200) {
+        console.log('Failed to retrieve targets')
+        return
+    }
+    console.log('Retrieved targets')
+
     console.log('~~~~~~ Hunt begins ~~~~~~')
     
     let results = []
@@ -18,24 +44,38 @@ let run = async () => {
     for (let target  of targets) {
         let listings = []
 
-        // identify the scraper to use
-        switch(target.site) {
-            case 'openrent':
-                listings = await openRentScraper(target.url)
-                break;
-            default:
-                // invalid site
-                console.log('Invalid target site:', target.site)
+        // fetch and parse listings
+        listings = await openRentScraper(target.url)
+        // ENABLE ONCE SUPPORT FOR OTHER SITES IS AVAILABLE
+        if (false) {
+            // identify the scraper to use
+            switch(target.site) {
+                case 'openrent':
+                    listings = await openRentScraper(target.url)
+                    break;
+                default:
+                    // invalid site
+                    console.log('Invalid target site:', target.site)
+            }
         }
 
         // append results
-        results.push({
-            site: target.site,
-            listings
-        })
+        results = [...results, ...listings]
     }
     
-    console.log('Findings', results)
+    console.log('~~~~~~ Submitting results ~~~~~~')
+
+    // submit data
+    let { data: submitResult, status: submitStatus } = await api.post('/listing', {
+        listings: results
+    })
+    // validate response
+    if (submitStatus != 200) {
+        console.log('Failed to submit results')
+        console.log('Status:', submitStatus)
+    } else {
+        console.log('Results submitted: ', submitResult)
+    }
 
     console.log('~~~~~~ Finished ~~~~~~')
 }
--- scrapers/openRentScraper.js
+++ scrapers/openRentScraper.js
@@ -8,7 +8,7 @@ module.exports = async function (targetUrl) {
     await page.goto(targetUrl)
     
     // parse the listings from the page
-    const listings = await page.evaluate(() => {
+    const listings = await page.evaluate(async () => {
         // get listings parent element
         let propertyData = document.getElementById('property-data')
         // get listing elements
@@ -18,19 +18,41 @@ module.exports = async function (targetUrl) {
         let results = []
 
         // parse each listing to the results
-        results = Object.values(children).map(c => {
+        for (let c of Object.values(children)) {
             // get price (p/month)
             let price = c.querySelector('div.pim').innerText.split(' ')[0]
+            price = parseInt(price.replace(/\D/g, ''))
+            
             // get listing name
             let name = c.querySelector('span.listing-title').innerText
+            
+            // get internal reference
+            let reference = c.id
+
+            // get listing info
+            let listingInfo = c.querySelector('ul.lic').children
+            // get bed count
+            let beds = parseInt(listingInfo[0].innerText.replace(/\D/g, ''))
+            // get bath count
+            let baths = parseInt(listingInfo[1].innerText.replace(/\D/g, ''))
+            // get cover image
+            let imgElement = c.querySelector('img.propertyPic') 
+            let image = imgElement.dataset.src || imgElement.src
+            if (imgElement.dataset.src) {
+                image = "https:" + image
+            }
 
             // return listing info
-            return {
+            results.push({
                 name,
                 price,
+                reference,
+                beds,
+                baths,
+                image,
                 link: c.href
-            }
-        })
+            })
+        }
 
         // return parsed listings
         return results
